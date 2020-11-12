7be17998e32dbce1920544d634dab72f98e4b678
guytwem <guytwem@gmail.com>
2020-11-12 13:00:54 +1100

    started functionality

---
+++ Assets/Scripts/Life.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Life : MonoBehaviour
+{
+    
+    public float minDistanceToWaypoint;
+    public float speed = 5.0f;
+    
+
+    
+
+    
+
+    public virtual void Move(GameObject gameObject, Vector2 targetPosition)
+    {
+        gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, targetPosition, speed * Time.deltaTime);
+    }
+    
+
+    
+
+    
+}
---
+++ Assets/Scripts/Prey.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prey : Life
+{
+
+    public GameObject prey;
+    public float health = 100;
+    public Predator predatorClass;
+
+    [SerializeField] private GameObject[] preyWaypointArray;
+    private int currentWaypoint = 0;
+    [SerializeField] private float minEvadeDistance = 3f;
+
+    
+
+    public void Start()
+    {
+        NextState();
+    }
+
+    private State preyState;
+
+    private enum State { Wander, Evade }
+
+    private IEnumerator WanderState()
+    {
+        Debug.Log("Prey Wandering");
+        while (preyState == State.Wander)
+        {
+            Wander();
+            yield return null;
+            if (Vector2.Distance(prey.transform.position, predatorClass.predator.transform.position) < minEvadeDistance)
+            {
+                preyState = State.Evade;
+            }
+        }
+        NextState();
+    }
+
+    private IEnumerator EvadeState()
+    {
+        Debug.Log("Prey Evading");
+        while (preyState == State.Evade)
+        {
+            Evade();
+            yield return null;
+            if (Vector2.Distance(prey.transform.position, predatorClass.predator.transform.position) > minEvadeDistance)
+            {
+                preyState = State.Wander;
+            }
+        }
+        NextState();
+    }
+
+    private void Wander()
+    {
+        float distance = Vector2.Distance(prey.transform.position, preyWaypointArray[currentWaypoint].transform.position);
+        if (distance < minDistanceToWaypoint)
+        {
+            currentWaypoint++;
+        }
+        if (currentWaypoint >= preyWaypointArray.Length)
+        {
+            currentWaypoint = 0;
+        }
+
+        Move(prey, preyWaypointArray[currentWaypoint].transform.position);
+    }
+
+    private void Evade()
+    {
+        Vector2 runAway = prey.transform.position - predatorClass.predator.transform.position;
+
+        if (Vector2.Distance(prey.transform.position, predatorClass.predator.transform.position) <= minEvadeDistance)
+        {
+            Move(prey, runAway);
+            
+        }
+        else
+        {
+            Wander();
+        }
+    }
+
+    private void NextState()
+    {
+        string methodName = preyState.ToString() + "State";
+        System.Reflection.MethodInfo info =
+            GetType().GetMethod(methodName,
+                                   System.Reflection.BindingFlags.NonPublic |
+                                   System.Reflection.BindingFlags.Instance);
+
+        StartCoroutine((IEnumerator)info.Invoke(this, null));
+    }
+
+    public void Die()
+    {
+        Destroy(prey);
+    }
+    //flock
+    //wander
+    //evade
+    //hide
+}
